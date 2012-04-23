b5823bd5f5e059197056588b824a7ea01a930f12
Honza Kral <honza.kral@gmail.com>
2012-04-23 17:16:03 +0200

    ignore git and the brand script when branding

--- brand.sh
+++ brand.sh
@@ -8,13 +8,13 @@ APP_NAME=${1:?brand.sh APP_NAME}
 cd $(dirname $(which "$0"))
 
 echo "Moving directories"
-find . -type d | grep -v './.git/' | tac | grep 'APP_NAME' | while read d
+find . -type d -not -name "brand.sh" -not -path './.git/*' | tac | grep 'APP_NAME' | while read d
 do
     echo "$d -> ${d//APP_NAME/$APP_NAME}"
     git mv "$d" "${d//APP_NAME/$APP_NAME}"
 done
 
 echo "Editting files"
-find . -type f | grep -v './.git/' | xargs sed -i -e "s;APP_NAME;$APP_NAME;g"
+find . -type f -not -name "brand.sh" -not -path './.git/*' | xargs sed -i -e "s;APP_NAME;$APP_NAME;g"
 
 echo "Done"
