2b6d573d14ea3e0059264e5e8e2fcdcfbbf11830
Honza Kral <honza.kral@gmail.com>
2012-04-23 17:27:34 +0200

    only rename last dir in the hierarchy so that nested dirs will work

--- brand.sh
+++ brand.sh
@@ -9,8 +9,8 @@ cd $(dirname $(which "$0"))
 echo "Moving directories"
 find . -type d -not -name "brand.sh" -not -path './.git/*' | tac | grep 'APP_NAME' | while read d
 do
-    echo "$d -> ${d//APP_NAME/$APP_NAME}"
-    git mv "$d" "${d//APP_NAME/$APP_NAME}"
+    echo "$d -> ${d/%APP_NAME/$APP_NAME}"
+    git mv "$d" "${d/%APP_NAME/$APP_NAME}"
 done
 
 echo "Editting files"
