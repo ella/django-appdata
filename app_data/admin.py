a61e2f4e4f7f6124be00e3342a1723ad42af89aa
Honza Kral <honza.kral@gmail.com>
2012-09-13 16:06:33 -0700

    don't construct multiform in Admin, use one given to us

--- app_data/admin.py
+++ app_data/admin.py
@@ -1,11 +1,9 @@
 from django.contrib.admin import ModelAdmin
 
-from .forms import multiform_factory
-
 class AppDataModelAdmin(ModelAdmin):
-    app_data_field = 'app_data'
-    app_data = {}
+    multiform = None
 
     def get_form(self, request, obj=None, **kwargs):
-        ModelForm = super(AppDataModelAdmin, self).get_form(request, obj, **kwargs)
-        return multiform_factory(ModelForm, app_data_field=self.app_data_field, **self.app_data)
+        if self.multiform is not None:
+            return self.multiform
+        return super(AppDataModelAdmin, self).get_form(request, obj=None, **kwargs)
