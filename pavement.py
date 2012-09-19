f47246127d8296766b54a377a7bba6a99340fcb7
Jakub Vysoky <jakub@borka.cz>
2012-09-19 09:59:34 +0200

    more packaging stuff

--- pavement.py
+++ pavement.py
@@ -1,6 +1,7 @@
 from paver.easy import path, options
 from paver.setuputils import setup
 
+
 VERSION = (0, 0, 1)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
@@ -9,18 +10,25 @@ f = (path(__file__).dirname() / 'README.rst').open()
 long_description = f.read().strip()
 f.close()
 
+f = (path(__file__).dirname() / 'requirements.txt').open()
+install_requires = f.read().strip()
+f.close()
+
 
 options(
     setup = dict(
-    name = 'app_data',
-    description = "app_data",
+    name = 'django-appdata',
+    description = "Extandable field that enables Django apps to store their data on your models.",
     url = "https://github.com/ella/django-appdata/",
     long_description = long_description,
     version = __versionstr__,
-    author = "Ella team",
+    author = 'Ella Development Team',
+    author_email = 'dev@ellaproject.cz',
+    license='BSD',
     packages = ['app_data'],
     zip_safe = False,
     include_package_data = True,
+    install_requires = install_requires,
     test_suite = 'test_app_data.run_tests.run_all',
     classifiers = [
         "Development Status :: 4 - Beta",
@@ -30,3 +38,4 @@ options(
 )
 
 setup(**options.setup)
+
