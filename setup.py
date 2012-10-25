7b28491d84645d1ea0935da534836c6b8bc492bc
Honza Kral <honza.kral@gmail.com>
2012-10-25 15:48:07 +0200

    setup.py without paver

--- setup.py
+++ setup.py
@@ -1,11 +1,43 @@
-try:
-    import paver.tasks
-except ImportError:
-    import os
-    if os.path.exists("paver-minilib.zip"):
-        import sys
-        sys.path.insert(0, "paver-minilib.zip")
-    import paver.tasks
+from os.path import join, dirname
+from setuptools import setup
 
 
-paver.tasks.main()
+VERSION = (0, 1, 1)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+f = open(join(dirname(__file__), 'README.rst'))
+long_description = f.read().strip()
+f.close()
+
+install_requires = [
+    'Django',
+    'south>=0.7',
+]
+test_requires = [
+    'nose',
+    'coverage',
+]
+
+setup(
+    name = 'django-appdata',
+    description = "Extandable field that enables Django apps to store their data on your models.",
+    url = "https://github.com/ella/django-appdata/",
+    long_description = long_description,
+    version = __versionstr__,
+    author = 'Ella Development Team',
+    author_email = 'dev@ellaproject.cz',
+    license='BSD',
+    packages = ['app_data'],
+    zip_safe = False,
+    include_package_data = True,
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+
+    test_suite='test_app_data.run_tests.run_all',
+    test_requires=test_requires,
+)
