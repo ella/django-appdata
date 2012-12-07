c2194452d25e2e652bb21a2031f057e79597594c
Honza Kral <honza.kral@gmail.com>
2012-12-07 18:52:59 +0100

    construct form_opts from declared_fieldsets

--- app_data/admin.py
+++ app_data/admin.py
@@ -1,17 +1,79 @@
+from functools import partial
+
+from django.contrib.admin.util import flatten_fieldsets
 from django.contrib.admin.options import ModelAdmin, InlineModelAdmin
 
-class AppDataModelAdmin(ModelAdmin):
-    multiform = None
+from app_data.forms import multiform_factory, MultiForm
 
+class AppDataAdminMixin(object):
+
+    multiform = MultiForm
+    app_form_opts = {}
+
+    def _get_form_factory_opts(self, request, obj=None, **kwargs):
+        if self.declared_fieldsets:
+            fields = flatten_fieldsets(self.declared_fieldsets)
+        else:
+            fields = None
+        if self.exclude is None:
+            exclude = []
+        else:
+            exclude = list(self.exclude)
+        exclude.extend(self.get_readonly_fields(request, obj))
+        if self.exclude is None and hasattr(self.form, '_meta') and self.form._meta.exclude:
+            # Take the custom ModelForm's Meta.exclude into account only if the
+            # ModelAdmin doesn't define its own.
+            exclude.extend(self.form._meta.exclude)
+
+        # construct the form_opts from declared_fieldsets
+        form_opts = self.app_form_opts.copy()
+        if fields is not None:
+            # put app_name prefixed fields into form_opts
+            for f in fields:
+                if '.' not in f:
+                    continue
+                label, name = f.split('.')
+                app_fields = form_opts.setdefault(label, {}).setdefault('fields', [])
+                if name not in app_fields:
+                    app_fields.append(name)
+            # .. and remove them from fields for the model form
+            fields = [f for f in fields if '.' not in f]
+
+        # do the same for exclude
+        for f in exclude:
+            if '.' not in f:
+                continue
+            label, name = f.split('.')
+            app_fields = form_opts.setdefault(label, {}).setdefault('exclude', [])
+            if name not in app_fields:
+                app_fields.append(name)
+        exclude = [f for f in exclude if '.' not in f]
+
+        # if exclude is an empty list we pass None to be consistant with the
+        # default on modelform_factory
+        exclude = exclude or None
+        defaults = {
+            "form": self.form,
+            "multiform": self.multiform,
+            "form_opts": form_opts,
+            "fields": fields,
+            "exclude": exclude,
+            "formfield_callback": partial(self.formfield_for_dbfield, request=request),
+        }
+        defaults.update(kwargs)
+        return defaults
+
+
+class AppDataModelAdmin(AppDataAdminMixin, ModelAdmin):
     def get_form(self, request, obj=None, **kwargs):
-        if self.multiform:
-            return self.multiform
-        return super(AppDataModelAdmin, self).get_form(request, obj=None, **kwargs)
+        """
+        Returns a Form class for use in the admin add view. This is used by
+        add_view and change_view.
+        """
+        return multiform_factory(self.model, **self._get_form_factory_opts(request, obj, **kwargs))
 
-
-class AppDataInlineModelAdmin(InlineModelAdmin):
-    multiform = None
-
+class AppDataInlineModelAdmin(AppDataAdminMixin, InlineModelAdmin):
+    pass
 
 class AppDataStackedInline(AppDataInlineModelAdmin):
     template = 'admin/edit_inline/stacked.html'
--- test_app_data/admin.py
+++ test_app_data/admin.py
@@ -1,24 +1,16 @@
 from django.contrib.admin import site
 
 from app_data.admin import AppDataModelAdmin, AddDataTabularInline
-from app_data.forms import multiform_factory
 
 from .models import Article, Author
 
-app_data = {
-    'publish': { 'exclude': ['publish_to'] },
-    'rss': { 'fields': ['title', 'author',] }
-}
-ArticleMultiForm = multiform_factory(Article, form_opts=app_data)
-AuthorMultiForm = multiform_factory(Author, form_opts={'personal': {'fields': ['first', 'last']}})
-
 class AuthorInline(AddDataTabularInline):
     model = Author
-    multiform = AuthorMultiForm
+    #declared_fieldsets = [
+    #    ('Personal', {'fields': [('personal.first', 'personal.last')]})
+    #]
 
 class ArticleModelAdmin(AppDataModelAdmin):
-    multiform = ArticleMultiForm
-
     declared_fieldsets = [
         (None, {'fields': ['rss.title']}),
         ('Meta', {'fields': ['rss.author']}),
