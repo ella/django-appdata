9e2ec4c2d8d3550027ae4a1898a2c08ef9d59e98
Honza Kral <honza.kral@gmail.com>
2012-12-19 01:53:12 +0100

    Ah, the woes of auto-complete - another typ occurence

--- test_app_data/admin.py
+++ test_app_data/admin.py
@@ -1,10 +1,10 @@
 from django.contrib.admin import site
 
-from app_data.admin import AppDataModelAdmin, AddDataTabularInline
+from app_data.admin import AppDataModelAdmin, AppDataTabularInline
 
 from .models import Article, Author
 
-class AuthorInline(AddDataTabularInline):
+class AuthorInline(AppDataTabularInline):
     model = Author
     declared_fieldsets = [
         ('Personal', {'fields': [('personal.first_name', 'personal.last_name')]})
